3766ff234b5e4e2f8c62a3d56a327ab1a43fa735
Max <mgoodman5bb@gmail.com>
2022-01-13 08:59:23 -0500

    speech recognition ready

--- python/language-emotion.py
+++ python/language-emotion.py
@@ -1,1 +1,20 @@
-print("hello!")
+import speech_recognition as sr
+import pyaudio
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    print("Say something...")
+    audio_data = r.record(source, duration = 5)
+    
+    print("")
+    print("Recognizing...")
+
+    try:
+        text = r.recognize_google(audio_data)
+    
+    except Exception:
+        print("")
+        print("Could not understand you, please try again!")
+
+    print(text)
